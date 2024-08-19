51beb9d0d09e2ccc873dc84e2a7abc2e904a176b
Dhananjay Sharma <dhananjay7862@gmail.com>
2024-08-19 11:34:42 +0530

    Add files via upload

---
+++ StyleDemo/src/main/java/com/example/demo/StyleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleDemoApplication.class, args);
+	}
+
+}
